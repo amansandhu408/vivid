a3889ce2d6fa0ef70fb9cd29d98569d615075486
amansandhu408 <amansandhu2241@gmail.com>
2025-07-03 01:17:34 +0530

    created prisma schema and dashboard page

---
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
@@ -0,0 +1,32 @@
+import { getAllProjects } from "@/actions/projects";
+import NotFound from "@/components/global/not-found";
+import Projects from "@/components/global/projects";
+import React from "react";
+
+const DashboardPage = async () => {
+  const allProjects = await getAllProjects();
+
+  return (
+    <div className="w-full flex flex-col gap-6 relative md:p-0 p-4">
+      <div className="flex flex-col-reverse items-start w-full gap-6 sm:flex-row sm:justify-between sm:items-center">
+        <div className="flex flex-col items-start">
+          <h1 className="text-2xl font-semibold dark:text-primary backdrop-blur-lg">
+            Projects
+          </h1>
+          <p className="text-base font-normal dark:text-gray-400">
+            Check All Your Unique Projects
+          </p>
+        </div>
+      </div>
+
+      {/* Return all projects */}
+      {allProjects.data && allProjects.data.length > 0 ? (
+        <Projects projects={allProjects.data} />
+      ) : (
+        <NotFound />
+      )}
+    </div>
+  );
+};
+
+export default DashboardPage;
